f4dd74f4993ea67460e2d4249b0262d3cf6df4c2
gautyagi <gautyagi@cisco.com>
2017-08-30 11:30:27 +0530

    added callback

---
+++ callback.js
@@ -0,0 +1,32 @@
+var evenDoubler = function(v,callback){
+    var waitTime=Math.floor(Math.random()*1001);
+    if(v%2==0){
+        setTimeout(function(){
+            callback(
+                new Error("even number"), 5, waitTime
+                );
+            
+        },waitTime)
+    } else{
+        setTimeout(function(){
+            callback(null,v*2,waitTime);
+        },waitTime)
+    }
+    
+
+};
+
+
+var resultHandler= function(err,r,t){
+    if(err){
+        console.log("Error"+ err.message + "wait time " + t);
+    }else{
+        console.log(r + "wait time "+ t);
+    }
+    
+   
+};
+console.log("jjjjjjjjjjj");
+
+evenDoubler(2,resultHandler);
+console.log("-----------");
